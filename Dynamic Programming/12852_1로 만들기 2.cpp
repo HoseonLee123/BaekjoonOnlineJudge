60b41058258d57aacd68d8e6cb2b7d340743f663
HoseonLee <hoseon310@gmail.com>
2023-11-13 22:43:40 +0900

    Dynamic Programming_12852_1로 만들기 2 문제 풀이. Dynamic Programming_1463_1로 만들기 변수들 이름을 풀이과정의 의도와 맞게 수정.

--- Dynamic Programming/12852_1로 만들기 2.cpp
+++ Dynamic Programming/12852_1로 만들기 2.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
 	ifstream InputFile;
-	InputFile.open("Input/1463.txt");
+	InputFile.open("Input/12852.txt");
 
 	if (!InputFile.is_open())
 		cout << "Error! Failed to open the file!" << endl;
@@ -20,6 +20,7 @@ int main()
 		InputFile >> X;
 
 		vector<int> MinOperationCount(X + 1, 0);
+		vector<int> Connection(X + 1, 0);
 		for (int i = 2; i < X + 1; i++)
 		{
 			int Sub1Count = MinOperationCount[i - 1] + 1;
@@ -30,16 +31,37 @@ int main()
 			{
 				Div2Count = MinOperationCount[i / 2] + 1;
 			}
-
 			if (i % 3 == 0)
 			{
 				Div3Count = MinOperationCount[i / 3] + 1;
+			}
+
+			if (min({ Sub1Count, Div2Count, Div3Count }) == Sub1Count)
+			{
+				Connection[i] = i - 1;
+			}
+			else if (min({ Sub1Count, Div2Count, Div3Count }) == Div2Count)
+			{
+				Connection[i] = i / 2;
+			}
+			else
+			{
+				Connection[i] = i / 3;
 			}
 
 			MinOperationCount[i] = min({ Sub1Count, Div2Count, Div3Count });
 		}
 
 		cout << MinOperationCount[X] << endl;
+
+		int TargetIndex = X;
+		cout << X << ' ';
+		for (int i = 0; i < MinOperationCount[X]; i++)
+		{
+			cout << Connection[TargetIndex] << " ";
+			TargetIndex = Connection[TargetIndex];
+		}
+		cout << endl;
 	}
 
 

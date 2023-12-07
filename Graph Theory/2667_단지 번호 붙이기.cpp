6a059958fc043a4cdff38210a574d7d4485a19d9
HoseonLee <hoseon310@gmail.com>
2023-12-07 13:39:13 +0900

    Graph Theory_2667_단지 번호 붙이기 복습 DFS와 BFS 모두 구현.

--- Graph Theory/2667_단지 번호 붙이기.cpp
+++ Graph Theory/2667_단지 번호 붙이기.cpp
@@ -1,101 +1,127 @@
 #include <iostream>
-#include <fstream>
-#include <vector>
+#include <utility>
 #include <string>
+#include <vector>
 #include <queue>
 #include <algorithm>
 
 using namespace std;
 
+int                    MapSize; // 5~25
+vector<vector<bool>>   Map;
+vector<vector<int>>    Numbering;
+vector<pair<int, int>> DirectionBundle {
+    { 1,  0},
+    {-1,  0},
+    { 0,  1},
+    { 0, -1}
+};
+int DFS(const int& CurrentRow, const int& CurrentCol, const int& Marking, int& NumberOfMarking);
+int BFS(const int& StartRow, const int& StartCol, const int& Marking, int& NumberOfMarking);
+
 int main()
 {
-	ifstream InputFile;
-	InputFile.open("input/2667.txt");
+    cin >> MapSize;
 
-	if (!InputFile.is_open())
-		cout << "Error! Failed to open the file!" << endl;
+    Map       = vector<vector<bool>>(MapSize, vector<bool>());
+    Numbering = vector<vector<int>>(MapSize, vector<int>(MapSize, 0));
 
-	int TotalTestCase;
-	InputFile >> TotalTestCase;
-	for (int TestCase = 1; TestCase <= TotalTestCase; TestCase++)
-	{
-		int MapSize; // 5~25
-		InputFile >> MapSize;
+    for (auto& OneRow : Map)
+    {
+        string Temp;
+        cin >> Temp;
+        vector<bool> ColumnElements;
+        for (const auto& StringTempElement : Temp)
+        {
+            ColumnElements.push_back(StringTempElement - '0');
+        }
+        OneRow = ColumnElements;
+    }
 
-		vector<vector<bool>> ComplexMap(MapSize, vector<bool>(MapSize, false));
-		for (int Row = 0; Row < MapSize; Row++)
-		{
-			string OneLine;
-			InputFile >> OneLine;
-			for (int Col = 0; Col < MapSize; Col++)
-			{
-				ComplexMap[Row][Col] = OneLine[Col] - '0';
-			}
-		}
+    int         Marking = 1;
+    vector<int> NumberOfEachComplex;
+    for (int Row = 0; Row < MapSize; Row++)
+    {
+        for (int Col = 0; Col < MapSize; Col++)
+        {
+            if (Map[Row][Col] == true && Numbering[Row][Col] == 0)
+            {
+                int NumberOfMarking {0};
 
-		vector<vector<int>> ComplexMapNumbering(MapSize, vector<int>(MapSize, 0));
+                // NumberOfEachComplex.push_back(DFS(Row, Col, Marking, NumberOfMarking));
+                NumberOfEachComplex.push_back(BFS(Row, Col, Marking, NumberOfMarking));
 
-		int Numbering = 1;
-		int TotalNumbering = 0;
-		vector<int> NumberingSet;
-		queue<pair<int, int>> Target;
+                Marking++;
+            }
+        }
+    }
 
-		vector<pair<int, int>> DirectionSet = { {0, 1},{1, 0},{0, -1},{-1, 0} };
+    sort(NumberOfEachComplex.begin(), NumberOfEachComplex.end());
+    cout << NumberOfEachComplex.size() << endl;
+    for (const auto& Element : NumberOfEachComplex)
+    {
+        cout << Element << endl;
+    }
+}
 
-		for (int Row = 0; Row < MapSize; Row++)
-		{
-			for (int Col = 0; Col < MapSize; Col++)
-			{
-				// If there's a house and the house has not been numbered yet.
-				if (ComplexMap[Row][Col] == true && ComplexMapNumbering[Row][Col] == 0)
-				{
-					// BFS
-					int StartRow = Row;
-					int StartCol = Col;
-					Target.push({ StartRow,StartCol });
-					ComplexMapNumbering[StartRow][StartCol] = Numbering;
-					TotalNumbering = 1;
+int DFS(const int& CurrentRow, const int& CurrentCol, const int& Marking, int& NumberOfMarking)
+{
+    Numbering[CurrentRow][CurrentCol] = Marking;
+    NumberOfMarking++;
 
-					while (Target.empty() == false)
-					{
-						int CurrentRow = Target.front().first;
-						int CurrentCol = Target.front().second;
-						Target.pop();
+    for (const auto& Dir : DirectionBundle)
+    {
+        int NextRow = CurrentRow + Dir.first;
+        int NextCol = CurrentCol + Dir.second;
 
-						for (const pair<int, int>& Direction : DirectionSet)
-						{
-							int NextRow = CurrentRow + Direction.first;
-							int NextCol = CurrentCol + Direction.second;
+        // First condition: if the next is within a range.
+        // Second condition: if the next is a house.
+        // Third condition: if the next has not been numbered yet.
+        if ((NextRow >= 0 && NextRow < MapSize) &&
+            (NextCol >= 0 && NextCol < MapSize) &&
+            Map[NextRow][NextCol] == true &&
+            Numbering[NextRow][NextCol] == 0)
+        {
+            DFS(NextRow, NextCol, Marking, NumberOfMarking);
+        }
+    }
 
-							// First condition: if the next is within a range.
-							// Second condition: if the next is a house.
-							// Third condition: if the next has not been numbered yet.
-							if (((NextRow >= 0 && NextRow < MapSize) && (NextCol >= 0 && NextCol < MapSize))
-								&& ComplexMap[NextRow][NextCol] == true
-								&& ComplexMapNumbering[NextRow][NextCol] == 0)
-							{
-								Target.push({ NextRow,NextCol });
-								ComplexMapNumbering[NextRow][NextCol] = Numbering;
-								TotalNumbering++;
-							}
-						}
-					}
+    return NumberOfMarking;
+}
 
-					NumberingSet.push_back(TotalNumbering);
-					Numbering++;
-					TotalNumbering = 0;
-				}
-			}
-		}
+int BFS(const int& StartRow, const int& StartCol, const int& Marking, int& NumberOfMarking)
+{
+    queue<pair<int, int>> Waiting;
+    Waiting.push({StartRow, StartCol});
+    Numbering[StartRow][StartCol] = Marking;
+    NumberOfMarking++;
 
-		sort(NumberingSet.begin(), NumberingSet.end());
-		cout << "#" << TestCase << " " << endl;
-		cout << NumberingSet.size() << endl;
-		for (const int& Numbering : NumberingSet)
-		{
-			cout << Numbering << endl;
-		}
-	}
+    while (Waiting.empty() == false)
+    {
+        int CurrentRow = Waiting.front().first;
+        int CurrentCol = Waiting.front().second;
+        Waiting.pop();
 
-	return 0;
+        for (const auto& Dir : DirectionBundle)
+        {
+            int NextRow = CurrentRow + Dir.first;
+            int NextCol = CurrentCol + Dir.second;
+
+            // First condition: if the next is within a range.
+            // Second condition: if the next is a house.
+            // Third condition: if the next has not been numbered yet.
+            if ((NextRow >= 0 && NextRow < MapSize) &&
+                (NextCol >= 0 && NextCol < MapSize) &&
+                Map[NextRow][NextCol] == true &&
+                Numbering[NextRow][NextCol] == 0)
+            {
+                Numbering[NextRow][NextCol] = Marking;
+                NumberOfMarking++;
+
+                Waiting.push({NextRow, NextCol});
+            }
+        }
+    }
+
+    return NumberOfMarking;
 }
